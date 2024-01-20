855d770685754cd5eddafea917e18c1a98c51803
AmoghParulekar2003 <119802348+AmoghParulekar2003@users.noreply.github.com>
2024-01-21 00:44:47 +0530

    Create ThalaForAReason.java

---
+++ Beginner/ThalaForAReason.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class ThalaForAReason
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		
+		if(n==7) {
+		    System.out.println("THALA");
+		} else {
+		    System.out.println("SADGE");
+		}
+	}
+}
