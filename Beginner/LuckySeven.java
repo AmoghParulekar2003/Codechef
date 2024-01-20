3d7bce932cdc75382077613541869fb48d5f366c
AmoghParulekar2003 <119802348+AmoghParulekar2003@users.noreply.github.com>
2024-01-21 00:30:04 +0530

    Create LuckySeven.java

---
+++ Beginner/LuckySeven.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class LuckySeven
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc = new Scanner(System.in);
+		String s = sc.next();
+		
+		char ch = s.charAt(6);
+		
+		System.out.println(ch);
+	}
+}
