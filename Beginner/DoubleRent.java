9c38204fc0990a0ea50f8273b304017e08da9086
AmoghParulekar2003 <119802348+AmoghParulekar2003@users.noreply.github.com>
2024-01-21 00:41:07 +0530

    Create DoubleRent.java

---
+++ Beginner/DoubleRent.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class DoubleRent
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc = new Scanner(System.in);
+		int x = sc.nextInt();
+		
+		int res = x*2;
+		System.out.println(res);
+	}
+}
