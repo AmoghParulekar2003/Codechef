9b6f460fae51ce257e295ddff68a8aaab4c59c09
AmoghParulekar2003 <119802348+AmoghParulekar2003@users.noreply.github.com>
2024-01-22 16:30:10 +0530

    Create Burgers.java

---
+++ Beginner/Burgers.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Burgers
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		
+		while(t-->0) {
+		    int a = sc.nextInt();
+		    int b = sc.nextInt();
+		    
+		    if(a <= b) {
+		        System.out.println(a);
+		    } else {
+		        System.out.println(b);
+		    }
+		}
+	}
+}
